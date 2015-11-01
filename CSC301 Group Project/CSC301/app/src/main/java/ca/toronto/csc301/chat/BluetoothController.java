6bf50e64e8d92320b989c6fe55579cc55f74eaf0
Akshay <akshayjain1996@gmail.com>
2015-10-31 22:08:21 -0400

    Imcomplete : Class to handel the connected device - handel IO

---
+++ CSC301 Group Project/CSC301/app/src/main/java/ca/toronto/csc301/chat/BluetoothController.java
@@ -0,0 +1,24 @@
+package ca.toronto.csc301.chat;
+
+/**
+ * Created by akshay on 31/10/15.
+ */
+public class BluetoothController {
+
+    BluetoothController(){
+
+    }
+
+    public void establishConnection(){
+
+    }
+
+    public void sendMessage(){
+
+    }
+
+    public void handelRecievedMessage(){
+
+    }
+
+}
